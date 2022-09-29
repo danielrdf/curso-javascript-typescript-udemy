db554be570a8119cdfb9f4f415e739fe57e73159
Daniel Fonseca <danielrdf@gmail.com>
2022-09-29 11:13:26 -0300

    Create index.js

---
+++ aula001/index.js
@@ -0,0 +1,8 @@
+console.log('Daniel Fonseca'); // String
+console.log("Daniel Fonseca");
+console.log(`Daniel Fonseca`);
+
+console.log(123456); // Number
+console.log(15.85);
+
+console.log('Daniel Fonseca', 41, 1.74);
